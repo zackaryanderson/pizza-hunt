dc3410208365e643947ab5e27d1becc5da5ba7d0
zanderson1998 <zanderson1998@yahoo.com>
2021-03-16 17:08:16 -0600

    add routes and front end fetch calls to pizza

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
-const apiRoutes = requrie('./api');
+const apiRoutes = require('./api');
 
 router.use('/', htmlRoutes);
-router.use('/', apiRoutes);
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(require('./routes'));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
     useFindAndModify: false,
     useNewUrlParser: true,
-    useUnfiedTopology: true
+    useUnifiedTopology: true
 });
 
 mongoose.set('debug',true);
