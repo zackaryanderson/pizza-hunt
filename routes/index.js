7d47ddc8c6832e57f5f3c694c1a8b2a65a811d13
zanderson1998 <zanderson1998@yahoo.com>
2021-03-16 16:06:25 -0600

    add pizza CRUD routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
+const apiRoutes = requrie('./api');
 
 router.use('/', htmlRoutes);
+router.use('/', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
