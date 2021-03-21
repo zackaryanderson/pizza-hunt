0d3afaa6b09dfbe099c75114132126f4ed515f8b
zanderson1998 <zanderson1998@yahoo.com>
2021-03-21 12:54:57 -0600

    add ability to reply to comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,31 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+    {
+        //custom id to not mess with comment id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 const CommentSchema = new Schema({
     writtenBy: {
@@ -9,8 +36,24 @@ const CommentSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [ReplySchema],
+
+},
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
     }
+);
+
+//virtual to get number of replies
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
